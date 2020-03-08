3ef21b5aa95fdd1b7ee7b775292f5e40ebe289a4
Osbios <Frederik.Hofe@gmail.com>
2020-03-08 18:45:50 +0100

    nitpick todo corrections

--- src/glCompact/ThreadContext.cpp
+++ src/glCompact/ThreadContext.cpp
@@ -19,15 +19,15 @@
 #include "glCompact/ThreadContext.hpp"
 
 /*TODO
-  Big issue still to be solveed:
+    Big issue still to be solved:
 
-  In the case of a multi thread/multi context application,
-  if a thread has thread-global glCompact objects and gets destroeyed
-  the destructors call OpenGL functions to delete the underlaying GL objects.
-  If the context is already destroyed then the calls are silently ignored (OpenGL standard)
-  And we end up with memory leaks in form of left over gl objects!
+    In the case of a multi thread/multi context application,
+    if a thread has thread-global glCompact objects and gets destroyed
+    the destructors call OpenGL functions to delete the underlaying GL objects.
+    If the context is already destroyed then the calls are silently ignored (Defined behavior by the OpenGL standard!)
+    And we end up with memory leaks in form of left over gl objects!
 
-  A: In debug mode will check if thread has active context in every function that needs one. Otherwise throw error!
+    A: In debug mode will check if thread has active context in every function that needs one. Otherwise we can cause an error or warning!
 */
 
 namespace glCompact {
