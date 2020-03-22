f10f54fcda7d9d2b9d15d92c0482ab00b7f7e4ea
Osbios <Frederik.Hofe@gmail.com>
2020-03-22 15:01:35 +0100

    Use non ARB DSA gen function for texture ID

--- src/glCompact/TextureInterface.cpp
+++ src/glCompact/TextureInterface.cpp
@@ -923,17 +923,16 @@ namespace glCompact {
                     for (uint32_t i = z; i < z + texSize.z; ++i)
                         threadContextGroup->functions.glGetTexImage(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, mipmapLevel, componentsAndArrangement, componentsTypes, reinterpret_cast<void*>(dataOffset + cubeMapSideSize * i));
                 } else if (entireXY && threadContextGroup->extensions.GL_ARB_texture_view) {
-                    //GL_ARB_texture_storage is guaranteed to be present! GL_ARB_texture_storage_multisample is not relevant here.
                     uint32_t viewTexId = 0;
                     int32_t viewTarget = target == GL_TEXTURE_CUBE_MAP_ARRAY ? GL_TEXTURE_2D_ARRAY : target; //GL_TEXTURE_CUBE_MAP_ARRAY can not have an arbitary layer selection range, GL_TEXTURE_2D_ARRAY can!
-                    threadContextGroup->functions.glCreateTextures(viewTarget, 1, &viewTexId);
+                    threadContextGroup->functions.glGenTextures(1, &viewTexId);
                     threadContextGroup->functions.glTextureView(viewTexId, viewTarget, id, surfaceFormat->sizedFormat, mipmapLevel, 1, texOffset.z, texSize.z);
                     if (threadContextGroup->extensions.GL_ARB_direct_state_access) {
                         threadContextGroup->functions.glGetTextureImage(viewTexId, mipmapLevel, componentsAndArrangement, componentsTypes, maxCopySizeGuard, offsetPointer);
                     } else {
                         threadContext->cachedBindTexture(viewTarget, viewTexId);
                         threadContextGroup->functions.glGetTexImage(viewTarget, mipmapLevel, componentsAndArrangement, componentsTypes, offsetPointer);
-                        threadContext->cachedBindTexture(viewTarget, 0); //Not all drivers cleanly unbind objects that get deleted, so we do it ourself
+                        threadContext->cachedBindTexture(viewTarget, 0);
                     }
                     threadContextGroup->functions.glDeleteTextures(1, &viewTexId);
                 } else {
