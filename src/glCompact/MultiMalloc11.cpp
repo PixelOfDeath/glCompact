9e3c1fde090aa31ed0b93be2604edafd6e5437f4
Frederik vom Hofe <Frederik.Hofe@gmail.com>
2020-05-21 21:43:44 +0200

    MSVC _aligned_malloc is terrible, so remove it and just use default malloc with over allocation!

--- src/glCompact/MultiMalloc11.cpp
+++ src/glCompact/MultiMalloc11.cpp
@@ -4,8 +4,6 @@
     #include <cstdlib> //For aligned_alloc
 #elif defined(__GNUC__) || defined(__clang__)
     #include <malloc.h>
-#elif _MSC_VER
-    #include <malloc.h>
 #endif
 
 namespace glCompact {
@@ -14,10 +12,9 @@ namespace glCompact {
             return aligned_alloc(alignment, size);
         #elif defined(__GNUC__) || defined(__clang__)
             return memalign(alignment, size);
-        #elif _MSC_VER
-            return _aligned_malloc(alignment, size); //only works with _aligned_free, don't like that!
         #else
-            static_assert(0, "Don't know aligned malloc for this system");
+            //Just over allocate and the template will do take care of the alignment!
+            return malloc(size + alignment);
         #endif
     }
 
@@ -26,13 +23,7 @@ namespace glCompact {
     }
 
     void MultiMalloc11::free() {
-        #if __cplusplus >= 201703L //C++17 or higher
-            ::free(basePtr);
-        #elif defined(__GNUC__) || defined(__clang__)
-            ::free(basePtr);
-        #elif _MSC_VER
-            _aligned_free(basePtr);
-        #endif
+        ::free(basePtr);
         basePtr = 0;
     }
 }
