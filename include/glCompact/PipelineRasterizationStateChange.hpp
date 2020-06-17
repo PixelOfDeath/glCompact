fe58da964b4f1c2c772e233b90676bfc6774efa5
Osbios <Frederik.Hofe@gmail.com>
2020-06-17 16:45:33 +0200

    Fix a whopsy, bools need to be in a struct or all access the same bit

--- include/glCompact/PipelineRasterizationStateChange.hpp
+++ include/glCompact/PipelineRasterizationStateChange.hpp
@@ -5,14 +5,16 @@ namespace glCompact {
     struct PipelineRasterizationStateChange {
         union {
             uint8_t all = 0;
-            bool
-                viewportScissor:1,
-                triangleFace:1,
-                rgbaMask:1,
-                stencil:1,
-                depth:1,
-                blend:1,
-                attributeLayout:1;
+            struct {
+                bool
+                    viewportScissor:1,
+                    triangleFace:1,
+                    rgbaMask:1,
+                    stencil:1,
+                    depth:1,
+                    blend:1,
+                    attributeLayout:1;
+            }
         };
     };
 }
