449874a734b41a4a827950e52cde4d1edf6ccd44
Frederik Uje vom Hofe <Frederik.Hofe@pm.me>
2021-02-13 22:10:09 +0100

    Fix: PipelineInterface::..._markSlotChange respect Pipeline max bind counters

--- src/glCompact/PipelineInterface.cpp
+++ src/glCompact/PipelineInterface.cpp
@@ -1031,6 +1031,7 @@ namespace glCompact {
     void PipelineInterface::buffer_uniform_markSlotChange(
         int32_t slot
     ) {
+        if (slot >= buffer_uniform_count) return;
         buffer_uniform_changedSlotMin = minimum(buffer_uniform_changedSlotMin, slot);
         buffer_uniform_changedSlotMax = maximum(buffer_uniform_changedSlotMax, slot);
     }
@@ -1038,6 +1039,7 @@ namespace glCompact {
     void PipelineInterface::buffer_atomicCounter_markSlotChange(
         int32_t slot
     ) {
+        if (slot >= buffer_atomicCounter_count) return;
         buffer_atomicCounter_changedSlotMin = minimum(buffer_atomicCounter_changedSlotMin, slot);
         buffer_atomicCounter_changedSlotMax = maximum(buffer_atomicCounter_changedSlotMax, slot);
     }
@@ -1045,6 +1047,7 @@ namespace glCompact {
     void PipelineInterface::buffer_shaderStorage_markSlotChange(
         int32_t slot
     ) {
+        if (slot >= buffer_shaderStorage_count) return;
         buffer_shaderStorage_changedSlotMin = minimum(buffer_shaderStorage_changedSlotMin, slot);
         buffer_shaderStorage_changedSlotMax = maximum(buffer_shaderStorage_changedSlotMax, slot);
     }
@@ -1052,6 +1055,7 @@ namespace glCompact {
     void PipelineInterface::texture_markSlotChange(
         int32_t slot
     ) {
+        if (slot >= sampler_count) return;
         texture_changedSlotMin = minimum(texture_changedSlotMin, slot);
         texture_changedSlotMax = maximum(texture_changedSlotMax, slot);
     }
@@ -1059,6 +1063,7 @@ namespace glCompact {
     void PipelineInterface::sampler_markSlotChange(
         int32_t slot
     ) {
+        if (slot >= sampler_count) return;
         sampler_changedSlotMin = minimum(sampler_changedSlotMin, slot);
         sampler_changedSlotMax = maximum(sampler_changedSlotMax, slot);
     }
@@ -1066,6 +1071,7 @@ namespace glCompact {
     void PipelineInterface::image_markSlotChange(
         int32_t slot
     ) {
+        if (slot >= image_count) return;
         image_changedSlotMin = minimum(image_changedSlotMin, slot);
         image_changedSlotMax = maximum(image_changedSlotMax, slot);
     }
